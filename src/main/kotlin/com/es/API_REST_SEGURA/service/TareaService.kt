8323c615a89ab5df124674cb9524258c31126f9c
sebasdelalv340 <148891336+sebasdelalv340@users.noreply.github.com>
2025-03-11 12:06:52 +0100

    tareas admin

--- src/main/kotlin/com/es/API_REST_SEGURA/service/TareaService.kt
+++ src/main/kotlin/com/es/API_REST_SEGURA/service/TareaService.kt
@@ -23,15 +23,17 @@ class TareaService {
         var tareas = tareaRepository.getTareas(username.lowercase()).map { tarea ->
             dtoMapper.tareaEntityToDTO(tarea)
         }
-        if (authentication.name == username) {
+
+        if (authentication.authorities.any {it.authority == "ROLE_ADMIN"}) {
+            tareas = getAll()
+        } else if (authentication.name == username) {
             if(tareas.isEmpty()){
                 throw ForbiddenException("No hay tareas para $username")
             } else {
                 return tareas
             }
-        } else if (authentication.authorities.any {it.authority == "ROLE_ADMIN"}) {
-            tareas = getAll()
         }
+
         return tareas
     }
 
